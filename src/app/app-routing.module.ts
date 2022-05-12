d49c9d4b7efc66bc0449ab69848e1a58e9e3ff9d
Winnie Thembelihle Nhlapo <winnie.nhlapo@dadevs.co.za>
2022-05-12 16:15:34 +0200

    navigation bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    component:NavBarComponent,
+    path: 'navigation',
+    // Component: NavBarComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
