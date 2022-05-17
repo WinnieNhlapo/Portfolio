9f4517a44ca44cb2b65184e9b39e6349c06dd750
Winnie Thembelihle Nhlapo <winnie.nhlapo@dadevs.co.za>
2022-05-17 16:12:06 +0200

    Grid

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ContentComponent } from './components/content/content.component';
 
 
 const routes: Routes = [
-  {
-    component:NavBarComponent,
-    path: 'navigation',
-    // Component: NavBarComponent,
-  },
+  {path:'nav',component:NavBarComponent},
+  {path:'',component:ContentComponent}
 ];
 
 @NgModule({
